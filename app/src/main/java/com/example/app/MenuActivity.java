edbbc62ac01ea74c9eae9f3e2e9e1f989f2b9bf6
Krishmila <madbkjayasinghe97@gmail.com>
2019-08-03 13:45:38 +0530

    commit

--- app/src/main/java/com/example/app/MenuActivity.java
+++ app/src/main/java/com/example/app/MenuActivity.java
@@ -2,13 +2,53 @@ package com.example.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
 
+    Button btn1,btn2,btn3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu2);
+
+        btn1=findViewById(R.id.tc_customize);
+        btn2=findViewById(R.id.sd_customize);
+        btn3=findViewById(R.id.ps_customize);
+        Intent i1 =getIntent();
+        Intent i2=getIntent();
+        Intent i3=getIntent();
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i1 = new Intent(MenuActivity.this,custom.class);
+                startActivity(i1);
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i2 = new Intent(MenuActivity.this,custom.class);
+                startActivity(i2);
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i3 = new Intent(MenuActivity.this,custom.class);
+                startActivity(i3);
+            }
+        });
     }
 }
