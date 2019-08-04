14b7eb7ae6f27d2874395a7db6a9163bcdff16bc
Madara1997 <42792878+Madara1997@users.noreply.github.com>
2019-08-04 17:47:01 +0530

    bugfix02

--- app/src/main/java/com/example/app/MainCalendar.java
+++ app/src/main/java/com/example/app/MainCalendar.java
@@ -10,7 +10,7 @@ public class MainCalendar extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_maincalendar);
 
         customCalendarView = (CustomCalendarView)findViewById(R.id.custom_calndar_view);
     }
